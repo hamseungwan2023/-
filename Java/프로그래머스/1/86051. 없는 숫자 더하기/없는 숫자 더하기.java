476c794e88a1968c15004a7c98fa36c906d7b62c
함승완 <125807759+hamseungwan2023@users.noreply.github.com>
2024-10-26 19:38:46 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

--- Java/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ Java/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(int[] numbers) {
         int answer = 0;
-        int[] numList = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         int a = 0;
         int b = 0;
         for (int i = 0; i < numbers.length; i++) {
